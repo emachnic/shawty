7bee6f23b7861d71677af074be6a2efc0f2e6e5d
Evan Machnic <emachnic@gmail.com>
2015-08-16 11:39:58 -0400

    Finished separating sinatra

--- shawty.rb
+++ shawty.rb
@@ -1,56 +1,32 @@
-require 'rubygems'
-require 'sinatra'
-require 'redis'
+class Shawty < Sinatra::Base
+  configure do
+    enable :inline_templates
+  end
 
-$redis = Redis.new host: 'localhost', port: 6379
+  $redis = Redis.new host: 'localhost', port: 6379
 
-get '/' do
-  erb :index
-end
-
-post '/' do
-  if (params[:url] =~ URI::regexp).nil?
-    @error = 'Nah man, please enter a valid URL.'
+  get '/' do
     erb :index
-  else
-    @token = (Time.now.to_i + rand(36**8)).to_s 36
-    $redis.set "links:#{@token}", params[:url]
-    erb :shortened
+  end
+
+  post '/' do
+    if (params[:url] =~ URI::regexp).nil?
+      @error = 'Nah man, please enter a valid URL.'
+      erb :index
+    else
+      @token = (Time.now.to_i + rand(36**8)).to_s 36
+      $redis.set "links:#{@token}", params[:url]
+      erb :shortened
+    end
+  end
+
+  get '/:token/?' do
+    url = $redis.get "links:#{params[:token]}"
+    if url.nil?
+      @error = "Sorry, ain't no URL there."
+    else
+      redirect url
+    end
   end
 end
 
-get '/:token/?' do
-  url = $redis.get "links:#{params[:token]}"
-  if url.nil?
-    @error = "Sorry, ain't no URL there."
-  else
-    redirect url
-  end
-end
-
-__END__
-
-@@ layout
-<!doctype html>
-<html>
-  <head>
-    <title>Shawty</title>
-  </head>
-  <body>
-    <%= yield %>
-  </body>
-</html>
-
-@@ index
-<h1>Shawty</h1>
-<% if @error %>
-  <p><%= @error %></p>
-<% end %>
-<form action='/' method='post'>
-  <input type='url' name='url' size='40' placeholder='Enter a URL' />
-  <input type='submit' value='Shorten' />
-</form>
-
-@@ shortened
-<h1>Aw yeah shawty! Here's your link:</h1>
-<p><a href="http://localhost:4567/<%= @token %>">http://localhost:4567/<%= @token %></a></p>
